813542986024491efff9777cbcdbab2270448290
Joan Toro Ortiz <joantoro@me.com>
2024-04-30 09:30:12 -0400

    Add product in local storage

--- jQuery/js/producto.js
+++ jQuery/js/producto.js
@@ -28,20 +28,16 @@ $(document).ready(function(){
             return false; // Previene la recarga de la página
         }
     })
+    loadProducts();
 });
 
 function addProduct(){
     var productName = $('#productName').val();
     var productPrice = $('#productPrice').val();
-    $('#productsTable tbody').append(`
-        <tr>
-            <td>${productName}</td>
-            <td>${productPrice}</td>
-            <td>
-                <button class="btn btn-primary btn-sm edit-product" onclick="editProduct(this)">Editar</button>
-                <button class="btn btn-danger btn-sm delete-product" onclick="deleteProduct(this)">Eliminar</button>
-            </td>
-    `);
+    var product = { Name: productName, Price: productPrice}
+
+    appendProductToTable(productName, productPrice);
+    saveProductToStorage(product);
 }
 
 function editProduct(button){
@@ -53,22 +49,67 @@ function editProduct(button){
         $(button).text('Guardar').removeClass('btn-info').addClass('btn-success');
         $(button).next().text('Cancelar').removeClass('btn-danger').addClass('btn-warning');
     } else { // Guardar
-        $(cols[0]).text($(cols[0]).find('input').val());
-        $(cols[1]).text($(cols[1]).find('input').val());
-        $(button).text('Editar').removeClass('btn-success').addClass('btn-primary');
+        var newName = $(cols[0]).find('input').val();
+        var newPrice = $(cols[1]).find('input').val();
+        $(cols[0]).text(newName);
+        $(cols[1]).text(newPrice);
+        $(button).text('Editar').removeClass('btn-success').addClass('btn-info');
         $(button).next().text('Eliminar').removeClass('btn-warning').addClass('btn-danger');
+        updateProductInStorage(row.index(), newName, newPrice);
     }
 }
 
+function updateProductInStorage(index, newName, newPrecio){
+    var products = JSON.parse(localStorage.getItem('products'));
+    products[index].Name = newName;
+    products[index].Price = newPrecio;
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
 function deleteProduct(button){
+    var row = $(button).closest('tr');
+    var cols = row.children('td');
     if(button.textContent === 'Cancelar'){
-        var row = $(button).closest('tr');
-        var cols = row.children('td');
         $(cols[0]).text($(cols[0]).find('input').val());
         $(cols[1]).text($(cols[1]).find('input').val());
-        $(button).prev().text('Editar').removeClass('btn-success').addClass('btn-info');
+        $(button).prev().text('Editar').removeClass('btn-info').addClass('btn-info');
         $(button).text('Eliminar').removeClass('btn-warning').addClass('btn-danger');
     } else {
-        $(button).closest('tr').remove();
+        removeFormStorage(row.index());
+        row.remove();
     }
+}
+
+function removeFormStorage(index) {
+    var products = JSON.parse(localStorage.getItem('products'));
+    products.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
+function loadProducts(){
+    if(localStorage.getItem('products')){
+        var products = JSON.parse(localStorage.getItem('products'));
+        products.forEach(function(product){
+            appendProductToTable(product.Name, product.Price);
+        });
+    }
+}
+
+function appendProductToTable(name, price){
+    $('#productsTable tbody').append(`
+        <tr>
+            <td>${name}</td>
+            <td>${new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(price)}</td>
+            <td>
+                <button class="btn btn-info btn-sm edit-product" onclick="editProduct(this)">Editar</button>
+                <button class="btn btn-danger btn-sm delete-product" onclick="deleteProduct(this)">Eliminar</button>
+            </td>
+    `);
+}
+
+function saveProductToStorage(product) {
+    var products = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+    //If Lineal > If condicion ? then algo : then otra cosa;
+    products.push(product);
+    localStorage.setItem('products', JSON.stringify(products));
 }
